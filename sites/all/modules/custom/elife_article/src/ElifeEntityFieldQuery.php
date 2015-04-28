d543e6a9056578dbd8c0fcdc2cf9afe1336d4980
nlisgo <nathan@aeopus.com>
2015-04-28 14:47:55 +0100

    Add xautoload module

---
+++ sites/all/modules/custom/elife_article/src/ElifeEntityFieldQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\elife_article\ElifeEntityFieldQuery.
+ */
+
+namespace Drupal\elife_article;
+
+
+class ElifeEntityFieldQuery extends \EntityFieldQuery {}
