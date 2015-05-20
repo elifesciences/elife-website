dbaabe383560f5eb8212f5c60ecb92562fe75a14
Luke Skibinski <l.skibinski@elifesciences.org>
2015-05-20 13:24:41 +0100

    added the run-tests.sh script that will run the behat tests in the elife-builder environment.

---
+++ run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /srv/ingestor/
+basedir=$PWD
+
+export BEHAT_PARAMS="{\"extensions\": {\"Drupal\\\\DrupalExtension\": {\"drupal\": {\"drupal_root\": \"$basedir\"}}}}"
+
+# Run behat tests
+cd $basedir/sites/all/tests/behat
+./bin/behat --config behat.travis.yml --tags='~@develop'
+
+cd $basedir
+sudo cat /var/log/apache2/error.log
+
+# Run simpletest
+# - drush en simpletest --yes
+# - drush cc all
+# - drush test-run 'eLife Services' --uri=http://127.0.0.1:80 --xml
+# - cd $basedir
