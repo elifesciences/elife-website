93a325fa5005c39ab2bc995f9d2271cc3e3a3d37
nlisgo <nathan@aeopus.com>
2015-06-30 09:47:21 +0100

    Ensure the tests exit with the expected result

--- run-tests.sh
+++ run-tests.sh
@@ -8,10 +8,17 @@ drush vset elife_article_ingest_citations 1 -y
 drush cc all
 
 # Run behat tests
+result=0
 cd $basedir/tests/behat
 ./bin/behat --format progress --tags='~@develop'
+rc=$?
+if [ $rc != 0 ] ; then
+    result=$rc
+fi
 
 # Delete flag to ingest citations
 cd $basedir/web
 drush vdel elife_article_ingest_citations -y
 drush cc all
+
+exit $result
