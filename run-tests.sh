470a9016b2225aa48ddb50b27d1d554472b06134
Chris Wilkinson <c.wilkinson@elifesciences.org>
2015-07-21 09:44:18 +0100

    Merge in non-Drupal composer.json files

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ basedir=$( cd $(dirname $0) ; pwd -P )
 # Run behat tests
 result=0
 cd $basedir/tests/behat
-./bin/behat --format progress --tags='~@develop'
+../../bin/bdpc --format progress --tags='~@develop'
 rc=$?
 if [ $rc != 0 ] ; then
     result=$rc
