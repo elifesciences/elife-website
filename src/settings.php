7fa2571b9183e0e016cb59597215d9384618f1b9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2015-07-29 10:21:29 +0100

    Enable Pathologic and feature-ise text formats

--- src/settings.php
+++ src/settings.php
@@ -10,7 +10,22 @@ $conf['allow_authorize_operations'] = FALSE;
 // Set path for 403.
 $conf['site_403'] = 'm4032404';
 
+// Set up Pathologic.
+$conf['pathologic_protocol_style'] = 'full';
+$conf['pathologic_local_paths'] = [
+  'http://elifesciences.org',
+  'https://elifesciences.org',
+  'http://www.elifesciences.org',
+  'https://www.elifesciences.org',
+  '/',
+];
+
 // Include the local settings, this MUST contain the $databases variable (and
 // any other sensitive credentials in the future), as well as any custom
 // configuration required during development.
 require __DIR__ . '/../local.settings.php';
+
+// Turn Pathologic setting into the expected string.
+if (isset($conf['pathologic_local_paths'])) {
+  $conf['pathologic_local_paths'] = implode("\n", $conf['pathologic_local_paths']);
+}
