c277aedd00a3d6cf5e71b557d7f088f55e4877cd
nlisgo <nathan@aeopus.com>
2015-08-05 22:09:48 +0100

    Introduce the toolbox ctools plugin

---
+++ src/elife_profile/modules/custom/elife_templates/js/elife_article_citation.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+  var url = Drupal.settings.elife_article_toolbox.url;
+  $('.st_reddit').after('<span class="st-mendeley"><a class="stButton" target="_blank" href="http://www.mendeley.com/import/?' + url + '"><span class="chicklets mendeley">Mendeley</span></a></span>');
+  
+  $('.api_button').click(function(){
+    $('#api_box').toggle('fast');
+    return false;
+  });
+ 
+  
+  
+  $('.citation-links > li').hover(function(){
+    $(this).toggleClass('show-article-toolbox-tooltip');
+  }, function(){
+    $(this).toggleClass('show-article-toolbox-tooltip');
+  });
+  
+$('.print').click(function() {
+    $('.ctools-collapsible-content').show();
+    window.print();
+    return false;
+  });
+});
