b967d9e407c1e48718f40cc18269105aecb98b64
Luke Skibinski <l.skibinski@elifesciences.org>
2015-08-12 12:04:23 +0100

    tweaked cronjob to run using the environment in /etc/profile

--- run-cron.sh
+++ run-cron.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash -l
+set -e # all commands must pass
+cd "$(dirname "$0")"
 
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-
-cd "$SCRIPTPATH"
-if [ -e ./web/sites/default/settings.php ]; then
-  cd web
-  drush core-cron
+if [ ! -e ./web/sites/default/settings.php ]; then
+    echo "could not find web/sites/default/settings.php"
+    exit 1;
 fi
+
+cd web
+drush core-cron
