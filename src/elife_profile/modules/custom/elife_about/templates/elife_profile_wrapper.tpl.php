02731ee5dcbe72205e8d5abe897182562f6a3a23
Chris Wilkinson <c.wilkinson@elifesciences.org>
2015-08-18 13:37:25 +0100

    Add person profiles to the about page

---
+++ src/elife_profile/modules/custom/elife_about/templates/elife_profile_wrapper.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * templates/elife_profile_wrapper.tpl.php
+ */
+?>
+<article class="aims-scope" id="aims-and-scope">
+  <header class="aims-scope__header">
+    <div class="aims-scope__would-be-hgroup">
+      <h2 class="aims-scope__main_heading">Aims &amp; scope</h2>
+
+      <p class="aims-scope__stand_first"><i>eLife</i> publishes outstanding
+        research in the life sciences and biomedicine, from the most fundamental
+        and theoretical work, through to translational, applied, and clinical
+        research.
+        Our <?php if ($senior_editors_count > 1): print $senior_editors_count; endif; ?>
+        Senior editors and
+        <?php if ($bre_count > 1): print $bre_count . '-member'; endif; ?> Board
+        of Reviewing Editors are among the most respected and accomplished
+        individuals in their fields &ndash; from human genetics and neuroscience
+        to biophysics and epidemiology.</p>
+    </div>
+  </header>
+  <section class="aims-scope__people_subjects" id="people">
+    <nav class="aims-scope__subject_list" id="subjectNav">
+      <?php print $menu; ?>
+    </nav>
+    <?php print $leadership; ?>
+    <?php print $regular; ?>
+  </section>
+</article>
