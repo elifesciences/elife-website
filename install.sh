8ced7f468be327889433a1378df23fe7f3d8b829
Chris Wilkinson <c.wilkinson@elifesciences.org>
2015-10-01 09:07:27 +0100

    Use Redis for caching

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
 cd "$SCRIPTPATH/web"
+redis-cli flushall
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush site-install elife_profile --account-name=admin --account-pass=password --site-name="eLife Sciences" --yes
--- tests/build/travis.local.settings.php
+++ tests/build/travis.local.settings.php
@@ -7,3 +7,6 @@ $conf['elife_node_binary'] = '%NODE_BIN%';
 
 // Disable logging as we're not reading it.
 $conf['elife_monolog_handlers'] = new NullHandler();
+
+$conf['redis_client_host'] = '%REDIS_HOST%';
+$conf['redis_client_port'] = %REDIS_PORT%;
