d9932f15e2b761a17bca3c327f31bbdb0b16acd9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2015-11-30 12:47:14 +0000

    Add Platform.sh configuration

---
+++ platform/local.settings.php
@@ -0,0 +1,36 @@
+<?php
+
+$platform = json_decode(base64_decode($_SERVER['PLATFORM_RELATIONSHIPS']), TRUE);
+
+$platform_database = reset($platform['database']);
+$platform_solr = reset($platform['solr']);
+$platform_redis = reset($platform['redis']);
+
+$databases = [
+  'default' =>
+    [
+      'default' =>
+        [
+          'database' => $platform_database['path'],
+          'username' => $platform_database['username'],
+          'password' => $platform_database['password'],
+          'host' => $platform_database['host'],
+          'port' => $platform_database['port'],
+          'driver' => $platform_database['scheme'],
+          'prefix' => '',
+        ],
+    ],
+];
+
+$conf['elife_cache_dir'] = sys_get_temp_dir() . '/elife';
+$conf['elife_environment'] = ELIFE_ENVIRONMENT_PRODUCTION;
+
+$conf['search_api_override_mode'] = 'default';
+$conf['search_api_override_servers']['elife_solr']['options']['host'] = $platform_solr['host'];
+$conf['search_api_override_servers']['elife_solr']['options']['port'] = $platform_solr['port'];
+$conf['search_api_override_servers']['elife_solr']['options']['path'] = '/' . ltrim($platform_solr['path'], '/');
+
+$conf['redis_client_host'] = $platform_redis['host'];
+$conf['redis_client_port'] = $platform_redis['port'];
+
+$conf['imagemagick_convert'] = '/usr/bin/convert';
