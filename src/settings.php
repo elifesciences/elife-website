95fc08ba9b450543a4bd0ed21df03d83f0f12621
nlisgo <nathan@aeopus.com>
2016-02-02 16:46:59 +0000

    Fix permissions for lagotto and access url for elife api

--- src/settings.php
+++ src/settings.php
@@ -94,7 +94,7 @@ $conf['lagotto_services_url'] = 'http://alm.svr.elifesciences.org';
 $conf['lagotto_services_apikey'] = '5pVvShVqAJ9Ch3tFzori';
 
 // Setup elife_article_almvis.
-$conf['elife_article_almvis_metrics_url'] = 'http://2015-09-03.api.elifesciences.org/proxy/metrics/api/v1/article/hw,ga/';
+$conf['elife_article_almvis_metrics_url'] = 'https://master.api.elifesciences.org/proxy/metrics/api/v1/article/hw,ga/';
 $conf['elife_article_almvis_ga_switch_date'] = '2015-11-11';
 
 // See https://www.drupal.org/node/2009584.
